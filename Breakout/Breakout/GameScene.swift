8a0f0ed62acf5cb53ddff9ea1281e5a3dfb81195
ASmallCowboy <72273535+ASmallCowboy@users.noreply.github.com>
2021-04-12 12:10:11 -0500

    Add files via upload

---
+++ Breakout/Breakout/GameScene.swift
@@ -0,0 +1,90 @@
+//
+//  GameScene.swift
+//  Breakout
+//
+//  Created by P.M. Student on 4/6/21.
+//
+
+import SpriteKit
+import GameplayKit
+import AVFoundation
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var ball:SKSpriteNode!
+    var paddle: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet{
+            scoreLabel.text = "Score:\(score)"
+        }
+    }
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "Ball")as? SKSpriteNode
+        paddle = self.childNode(withName: "Paddle")as? SKSpriteNode
+        scoreLabel = self.childNode(withName: "Score")as? SKLabelNode
+        
+        let sound = Bundle.main.path(forResource: "brick", ofType: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf : URL(fileURLWithPath: sound!))
+        } catch {
+            print(error)
+        }
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
+        border.friction = 0
+        self.physicsBody = border
+        
+        self.physicsWorld.contactDelegate = self
+        
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event:
+    UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.location(in:self)
+            paddle.position.x = touchLocation.x
+        }
+    }
+ 
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let touchLocation = touch.location(in:self)
+            paddle.position.x = touchLocation.x
+        }
+    }
+  
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bodyAName = contact.bodyA.node?.name
+        let bodyBName = contact.bodyB.node?.name
+
+        if bodyAName == "Ball" && bodyBName == "Brick" ||
+            bodyAName == "Brick" && bodyBName == "Ball" {
+            if bodyAName == "Brick" {
+                audioPlayer.play()
+                contact.bodyA.node?.removeFromParent()
+                score += 1
+            } else if bodyBName == "Brick" {
+                audioPlayer.play()
+                contact.bodyB.node?.removeFromParent()
+                score += 1
+            }
+        }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        if (score == 9) {
+            scoreLabel.text = "You win"
+        }
+        if (ball.position.y < paddle.position.y) {
+            scoreLabel.text = "You lose"
+            self.view?.isPaused = true
+        }
+    }
+}
